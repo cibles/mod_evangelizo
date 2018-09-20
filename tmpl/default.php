55ce5a6ca60c0319ef01cf3f475fa54400a6a8e0
Cibles <contact@cibles.fr>
2018-09-20 18:13:40 +0200

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,11 +1,12 @@
 <?php
 /**
  * @package     mod_evangelizo
- *
+ * @version     1.0
+ * @author      Nicolas Fischmeister - Agence web Cibles
+ * @link        http://www.cibles.fr
  * @copyright   Copyright (C) 2018 Agence web Cibles. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 3 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
 ?>
 
